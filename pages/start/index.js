47d69f0b8c47924e20019a28143f6468026d6669
xiangxuchen <xiangxuchen@sohu-inc.com>
2018-07-14 17:53:45 +0800

    first commit

---
+++ pages/start/index.js
@@ -0,0 +1,26 @@
+const App = getApp()
+
+Page({
+    data: {
+        indicatorDots: !1,
+        autoplay: !1,
+        current: 0,
+        interval: 3000,
+        duration: 1000,
+        circular: !1,
+    },
+    onLoad() {},
+    onShow() {},
+    bindload(e) {
+      setTimeout(App.WxService.getStorageSync('token') ? this.goIndex : this.goLogin, 5000)
+    },
+
+    //有token，一登录，跳转到首页
+    goIndex() {
+        App.WxService.switchTab('/pages/index/index')
+    },
+    //未登录时跳转到登录页面，没有token
+    goLogin() {
+        App.WxService.redirectTo('/pages/login/index')
+    },
+})
